e709e000f8506687ee3e70e67a07e7a500433323
Jacob Rapp <jake.rapp322@gmail.com>
2016-12-22 15:49:34 -0800

    Adds/removes gems and installs/configures Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Devise default url
+  # config.action_mailer.default_url_options = { host: 'wha't sthe actual url, port: 'port-integer' }
 end
