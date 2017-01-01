91a418a0095dceae059d4c64d6f08aa226ae29ed
Jacob Rapp <jake.rapp322@gmail.com>
2016-12-31 18:59:03 -0800

    Validates search name cannot be blank

--- app/models/search.rb
+++ app/models/search.rb
@@ -3,7 +3,7 @@ class Search < ApplicationRecord
   has_and_belongs_to_many :areas
   has_many :results
 
-  validates :name, uniqueness: true
+  validates :name, uniqueness: true, presence: true, allow_blank: false
 
   enum refresh_interval: { hourly: 0, daily: 1 }
 end
