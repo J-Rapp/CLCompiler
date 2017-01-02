3da59908f995d88ee98640ae45d3119a300e32ae
Jacob Rapp <jake.rapp322@gmail.com>
2017-01-01 21:56:01 -0800

    Polishes logic for executing single search

--- app/services/searches_service.rb
+++ app/services/searches_service.rb
@@ -2,20 +2,15 @@ require 'craigslist'
 
 class SearchesService
   def call(opts = {})
-    parse_and_persist_ready_searches(opts)
+    opts[:search] ? execute(opts[:search]) : execute_all_ready_searches
   end
 
   private
 
-  def parse_and_persist_ready_searches(opts = {})
-    searches = if opts[:user_id]
-                 Search.all.where(user_id: opts[:user_id])
-               else
-                 Search.all
-               end
+  def execute_all_ready_searches
+    searches = Search.all
     searches.each do |search|
-      craigslist_results = search.refresh_interval == 'daily' ? check(search) : execute(search)
-      ResultsService.new.persist(craigslist_results, search.id)
+      search.refresh_interval == 'daily' ? check(search) : execute(search)
       email_new_results(search) if search.results.deliverable.any?
     end
   end
@@ -34,7 +29,12 @@ class SearchesService
       min_price: search.min_price,
       max_price: search.max_price
     }
-    Craigslist.search(subdomains, params)
+    craigslist_results = Craigslist.search(subdomains, params)
+    persist(craigslist_results, search)
+  end
+
+  def persist(craigslist_results, search)
+    ResultsService.new.persist(craigslist_results, search.id)
   end
 
   def email_new_results(search)
