b0a06ca639e6e3213954cd39e7beefd4647a5cc7
Jacob Rapp <jake.rapp322@gmail.com>
2017-01-08 19:48:02 -0800

    Removes non-react form tags

--- app/assets/javascripts/components/password_form.js.jsx
+++ app/assets/javascripts/components/password_form.js.jsx
@@ -1,16 +1,16 @@
 var PasswordForm = React.createClass({
   render: function() {
     return (
-      <form class="new_user" id="new_user" action="/users/password" accept-charset="UTF-8" method="post">
+      <form id="new_user" action="/users/password" method="post">
         <input name="utf8" type="hidden" value="&#x2713;" />
         <input type="hidden" name="authenticity_token" value="pi/aj1NcBraBCaIEX56Bb59vYQh9wrBkozGX1uVxSByfz21iXwcN6WGYeDK0YFd9gMc5Tg3pHhDYV9T9Yo6N1A==" />
 
-        <div class="field">
-          <label for="user_email">Email</label><br />
-          <input autofocus="autofocus" type="email" value="" name="user[email]" id="user_email" />
+        <div>
+          <label>Email</label><br />
+          <input type="email" value="" name="user[email]" id="user_email" />
         </div>
 
-        <div class="actions">
+        <div>
           <input type="submit" name="commit" value="Send me reset password instructions" data-disable-with="Send me reset password instructions" />
         </div>
       </form>
--- app/assets/javascripts/components/registration_form.js.jsx
+++ app/assets/javascripts/components/registration_form.js.jsx
@@ -1,33 +1,33 @@
 var RegistrationForm = React.createClass({
   render: function () {
     return (
-      <form class="new_user" id="new_user" action="/users" accept-charset="UTF-8" method="post">
+      <form id="new_user" action="/users" method="post">
         <input name="utf8" type="hidden" value="&#x2713;" />
         <input type="hidden" name="authenticity_token" value="NENEEUJhl3i3ENJXNHF0xp3y22nOAWheiTf82z97Fp3YzXKxHXqOQfV4NowUlAKsE2UMUXXCRh8xhqUOBfx/yQ==" />
 
-        <div class='field'>
-          <label for="user_username">Username</label><br />
-          <input autofocus="autofocus" type="text" value="" name="user[username]" id="user_username" />
+        <div>
+          <label>Username</label><br />
+          <input type="text" value="" name="user[username]" id="user_username" />
         </div>
 
-        <div class='field'>
-          <label for="user_email">Email</label><br />
+        <div>
+          <label>Email</label><br />
           <input type="email" value="" name="user[email]" id="user_email" />
         </div>
 
-        <div class='field'>
-          <label for="user_password">Password</label>
+        <div>
+          <label>Password</label>
           <em>(6 characters minimum)</em>
           <br />
-          <input autocomplete="off" type="password" name="user[password]" id="user_password" />
+          <input type="password" name="user[password]" id="user_password" />
         </div>
 
-        <div class='field'>
-          <label for="user_password_confirmation">Password confirmation</label><br />
-          <input autocomplete="off" type="password" name="user[password_confirmation]" id="user_password_confirmation" />
+        <div>
+          <label>Password confirmation</label><br />
+          <input type="password" name="user[password_confirmation]" id="user_password_confirmation" />
         </div>
 
-        <div class='actions'>
+        <div>
           <input type="submit" name="commit" value="Sign up" data-disable-with="Sign up" />
         </div>
       </form>
