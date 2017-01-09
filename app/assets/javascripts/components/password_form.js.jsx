6799b59e9d0ac23173456c1a0871cce0ca4fe5a9
Jacob Rapp <jake.rapp322@gmail.com>
2017-01-09 13:38:18 -0800

    Adds basic content styling to welcome page

--- app/assets/javascripts/components/password_form.js.jsx
+++ app/assets/javascripts/components/password_form.js.jsx
@@ -1,19 +1,22 @@
 var PasswordForm = React.createClass({
   render: function() {
     return (
-      <form id="new_user" action="/users/password" method="post">
-        <input name="utf8" type="hidden" value="&#x2713;" />
-        <input type="hidden" name="authenticity_token" value={this.props.token} />
+      <div>
+        <hr />
+        <form id="new_user" action="/users/password" method="post">
+          <input name="utf8" type="hidden" value="&#x2713;" />
+          <input type="hidden" name="authenticity_token" value={this.props.token} />
 
-        <div>
-          <label>Email</label><br />
-          <input type="email" name="user[email]" />
-        </div>
+          <div>
+            <label>Email</label><br />
+            <input type="email" name="user[email]" />
+          </div>
 
-        <div>
-          <input type="submit" value="Email instructions" />
-        </div>
-      </form>
+          <div>
+            <input type="submit" value="Email instructions" />
+          </div>
+        </form>
+      </div>
     )
   }
 })
--- app/assets/javascripts/components/welcome_page.js.jsx
+++ app/assets/javascripts/components/welcome_page.js.jsx
@@ -2,8 +2,8 @@ var WelcomePage = React.createClass({
   getToken() {
     var metas = document.getElementsByTagName('meta');
     for (var i=0; i<metas.length; i++) {
-      if (metas[i].getAttribute("name") === 'csrf-token') {
-        return metas[i].getAttribute("content");
+      if (metas[i].getAttribute('name') === 'csrf-token') {
+        return metas[i].getAttribute('content');
       }
     }
   },
@@ -18,23 +18,44 @@ var WelcomePage = React.createClass({
   },
   render: function() {
     return (
-      <div className="container">
-        <h1>MegsList</h1>
-        <h2>Let Meg take care of it!</h2>
-        <p>
-          Automatically search multiple Craigslist cities at once,<br />
-          and get emails when new listings are posted!
-        </p>
-        <button onClick={(e) => this.handleLogin(e)}>
-          Login
-        </button>
-        <button onClick={(e) => this.handleRegistration(e)}>
-          Register
-        </button>
-        <button onClick={(e) => this.handlePassword(e)}>
-          I Forgot My Password
-        </button>
-        <div id='form-area'>
+      <div className='container content-box'>
+        <div className='row text-center'>
+          <div className='col-sm-12'>
+            <h1>MegsList</h1>
+          </div>
+        </div>
+        <hr />
+        <div className='row text-center'>
+          <div className='col-sm-12'>
+            <h2>Let Meg take care of it!</h2>
+          </div>
+          <div className='col-sm-12'>
+            <p>
+            Automatically search multiple Craigslist cities at once, 
+            and get emails when new listings are posted.
+            </p>
+          </div>
+        </div>
+        <div className='row text-center'>
+          <div className='col-sm-12'>
+            <button onClick={(e) => this.handleLogin(e)}>
+              Login
+            </button>
+          </div>
+          <div className='col-sm-12'>
+            <button onClick={(e) => this.handleRegistration(e)}>
+              Register
+            </button>
+          </div>
+          <div className='col-sm-12'>
+            <button onClick={(e) => this.handlePassword(e)}>
+              I Forgot My Password
+            </button>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col-sm-12' id='form-area'>
+          </div>
         </div>
       </div>
     )
