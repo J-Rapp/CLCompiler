1a2cdb1c26513db6f138927a99c51d7ebc47e9f9
Jacob Rapp <jake.rapp322@gmail.com>
2017-01-10 15:41:53 -0800

    Adds state for login form component

--- app/assets/javascripts/components/forms/login_form.js.jsx
+++ app/assets/javascripts/components/forms/login_form.js.jsx
@@ -1,28 +1,49 @@
-function LoginForm(props) {
-  return (
-    <form id='new_user' action='/users/sign_in' method='post'>
-      <input name='utf8' type='hidden' value='&#x2713;' />
-      <input type='hidden' name='authenticity_token' value={props.token} />
+class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      token: props.token,
+      email: '',
+      password: '',
+      isChecked: false
+    }
+  }
+  handleChange(event) {
+    this.setState({
+      [event.target.type]: event.target.value
+    })
+  }
+  toggleCheckbox() {
+    this.setState({
+      isChecked: !this.state.isChecked
+    })
+  }
+  render() {
+    return (
+      <form action='/users/sign_in' method='post'>
+        <input name='utf8' type='hidden' value='&#x2713;' />
+        <input type='hidden' name='authenticity_token' value={this.state.token} />
 
-      <div className='form-field'>
-        <label>Email</label><br />
-        <input type='email' name='user[email]' />
-      </div>
+        <div className='form-field'>
+          <label>Email</label><br />
+          <input type='email' name='user[email]' value={this.state.email} onChange={(e) => this.handleChange(e)} />
+        </div>
 
-      <div className='form-field'>
-        <label>Password</label><br />
-        <input type='password' name='user[password]' />
-      </div>
+        <div className='form-field'>
+          <label>Password</label><br />
+          <input type='password' name='user[password]' value={this.state.password} onChange={(e) => this.handleChange(e)} />
+        </div>
 
-      <div className='form-field'>
-        <input name='user[remember_me]' type='hidden' value='0' />
-        <input type='checkbox' name='user[remember_me]' />
-        <label>Remember Me</label>
-      </div>
+        <div className='form-field'>
+          <input name='user[remember_me]' type='hidden' value={this.state.isChecked ? '1' : '0'} />
+          <input type='checkbox' name='user[remember_me]' onChange={() => this.toggleCheckbox()} />
+          <label>Remember Me</label>
+        </div>
 
-      <div className='form-field'>
-        <input type='submit' className='btn btn-secondary' value='Log In' />
-      </div>
-    </form>
-  )
+        <div className='form-field'>
+          <input type='submit' className='btn btn-secondary' value='Log In' />
+        </div>
+      </form>
+    )
+  }
 }
