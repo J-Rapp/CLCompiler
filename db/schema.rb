f64e11ffd35ee31d2c277f95734be7f72c209588
Jacob Rapp <jake.rapp322@gmail.com>
2017-02-10 15:38:22 -0800

    Modifies schema file

--- db/schema.rb
+++ db/schema.rb
@@ -42,13 +42,13 @@ ActiveRecord::Schema.define(version: 20170102043906) do
   end
 
   create_table "listings", force: :cascade do |t|
-    t.string   "title",                     null: false
-    t.string   "url",                       null: false
-    t.string   "price",      default: "$0"
+    t.string   "title",      null: false
+    t.string   "url",        null: false
+    t.string   "price"
     t.boolean  "favorite"
     t.integer  "search_id"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["search_id"], name: "index_listings_on_search_id", using: :btree
   end
 
